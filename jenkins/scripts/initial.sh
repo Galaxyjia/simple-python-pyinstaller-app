dc4b36a0f30c9ec6bfa9e7f858afc474d0550f49
Galaxyjia <328956238@qq.com>
2020-08-20 07:25:00 +0800

    add set -x

--- jenkins/scripts/initial.sh
+++ jenkins/scripts/initial.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env sh
 
 echo '初始化脚本'
-
 echo '[python版本信息]'
-python --version
-
 echo '[当前路径]'
-pwd
-
 echo '[当前目录结构]'
+
+set -x
+python --version
 ls
 
 echo '[设置pip镜像源为阿里云]'
@@ -16,3 +14,5 @@ pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
 
 echo '[更新pip]'
 pip install --upgrade pip
+
+set +x
