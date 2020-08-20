49f9c1d18f418820fa26964fa8ff82c0029f87d6
Galaxyjia <328956238@qq.com>
2020-08-20 08:00:56 +0800

    shell test7

--- jenkins/scripts/test2.sh
+++ jenkins/scripts/test2.sh
@@ -13,6 +13,9 @@ set -x
 #  install --save-dev cross-env
 set +x
 python --version
+
+pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
+
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
