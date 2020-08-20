a5468946595965037325d4d4a60076e28431b48b
Galaxyjia <328956238@qq.com>
2020-08-20 08:02:20 +0800

    shell test8

--- jenkins/scripts/test2.sh
+++ jenkins/scripts/test2.sh
@@ -16,6 +16,7 @@ python --version
 
 pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
 
+pip install --upgrade pip
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
