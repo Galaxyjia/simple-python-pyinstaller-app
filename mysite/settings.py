6374f2ba31768e5d7c10d37c98cd396d25e0dfd4
Galaxyjia <328956238@qq.com>
2020-08-20 11:49:45 +0800

    add simple-ui

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,88 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# simpleui 设置
+
+# 首页配置
+# SIMPLEUI_HOME_PAGE = 'https://www.baidu.com'
+# 首页标题
+# SIMPLEUI_HOME_TITLE = '百度一下你就知道'
+# 首页图标,支持element-ui的图标和fontawesome的图标
+# SIMPLEUI_HOME_ICON = 'el-icon-date'
+
+# 设置simpleui 点击首页图标跳转的地址
+SIMPLEUI_INDEX = 'https://www.88cto.com'
+
+# 首页显示服务器、python、django、simpleui相关信息
+SIMPLEUI_HOME_INFO = False
+
+# 首页显示快速操作
+# SIMPLEUI_HOME_QUICK = False
+
+# 首页显示最近动作
+SIMPLEUI_HOME_ACTION = False
+
+# 自定义SIMPLEUI的Logo
+# SIMPLEUI_LOGO = 'https://avatars2.githubusercontent.com/u/13655483?s=60&v=4'
+
+# 登录页粒子动画，默认开启，False关闭
+# SIMPLEUI_LOGIN_PARTICLES = False
+
+# 让simpleui 不要收集相关信息
+SIMPLEUI_ANALYSIS = True
+
+# 自定义simpleui 菜单
+SIMPLEUI_CONFIG = {
+    # 在自定义菜单的基础上保留系统模块
+    'system_keep': True,
+    # 'menu_display':['Simpleui', '测试', '权限认证', '动态菜单测试'],
+    'menus': [{
+        'name': 'PCB跳转',
+        'icon': 'fas fa-adjust',
+        'url': 'https://gitee.com/tompeppa/simpleui'
+    }, {
+        'app': 'auth',
+        'name': '权限认证',
+        'icon': 'fas fa-user-shield',
+        'models': [{
+            'name': '用户',
+            'icon': 'fa fa-user',
+            'url': '/admin/auth/group/'
+        }]
+    }, {
+        'name': 'PCB跳转测试',
+        'icon': 'fa fa-file',
+        'models': [{
+            'name': 'PCB问题收集',
+            'url': 'http://baidu.com',
+            'icon': 'far fa-surprise'
+        }, {
+            'name': 'PCB内网穿透',
+            'url': 'https://www.wezoz.com',
+            'icon': 'fab fa-github'
+        }, {
+            'name': 'PCB内网穿透',
+            'url': 'https://www.wezoz.com',
+            'icon': 'fab fa-github'
+        }, {
+            'name': 'PCB登录页嵌套测试',
+            'url': '/login'
+        }]
+    }]
+}
+
+# 是否显示默认图标，默认=True
+# SIMPLEUI_DEFAULT_ICON = False
+
+# 图标设置，图标参考：
+SIMPLEUI_ICON = {
+    '系统管理': 'fab fa-apple',
+    '员工管理': 'fas fa-user-tie'
+}
+
+# 指定simpleui 是否以脱机模式加载静态资源，为True的时候将默认从本地读取所有资源，即使没有联网一样可以。适合内网项目
+# 不填该项或者为False的时候，默认从第三方的cdn获取
+
+SIMPLEUI_STATIC_OFFLINE = False
