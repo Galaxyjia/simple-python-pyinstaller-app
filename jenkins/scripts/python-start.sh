437fb1fde51b869b197fc74bee9d9dbceb9f7cef
Galaxyjia <328956238@qq.com>
2020-09-04 08:38:57 +0800

    fix kill again2

--- jenkins/scripts/python-start.sh
+++ jenkins/scripts/python-start.sh
@@ -20,6 +20,8 @@ pip3 list
 # BUILD_ID=DONTKILLME nohup python manage.py runserver 0.0.0.0:8000 &
 # BUILD_ID=DONTKILLME
 python manage.py runserver 0.0.0.0:8000 &
+sleep 1
+echo $! > .pidfile
 
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
