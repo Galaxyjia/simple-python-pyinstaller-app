dc17db52a6a13b76c955a6669643cb125af8907e
Galaxyjia <328956238@qq.com>
2020-09-04 08:22:28 +0800

    test server &

--- jenkins/scripts/python-start.sh
+++ jenkins/scripts/python-start.sh
@@ -17,7 +17,8 @@ pip3 install Django
 # pip3 install -r requirements.txt
 pip3 list
 
-BUILD_ID=DONTKILLME nohup python manage.py runserver 0.0.0.0:8000 &
+# BUILD_ID=DONTKILLME nohup python manage.py runserver 0.0.0.0:8000 &
+python manage.py runserver 0.0.0.0:8000 &
 
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
